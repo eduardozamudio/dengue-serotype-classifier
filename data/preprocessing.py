60e63f43e7bd987aa4afb633e9af7ef54ef0f647
Eduardo Zamudio <eduardozamudio@gmail.com>
2019-05-01 12:53:10 -0300

    Add relative paths

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -3,9 +3,9 @@ datos=""
 linea=""
 l=[]
 x=0
-datos=open("/home/sergiolitwiniuk/Documentos/clasificador_dengue/dengue-serotype-classifier/data/all_den_proteinE.fasta","r")
-salida = open("/home/sergiolitwiniuk/Documentos/clasificador_dengue/dengue-serotype-classifier/data/salida_proteinE.fasta","w")
-headers= open("/home/sergiolitwiniuk/Documentos/clasificador_dengue/dengue-serotype-classifier/data/headers_proteinE.fasta","w")
+datos=open("all_den_proteinE.fasta","r")
+salida = open("salida_proteinE.fasta","w")
+headers= open("headers_proteinE.fasta","w")
 for i in datos:
     if not re.match('>', i):
         i.replace("\n","")
